a3c4cf13daf616d81e40edfd5f1792d0b1c3b61c
hoover456 <hoover852@yahoo.com>
2016-04-04 22:22:48 -0400

    main.m now optimizes indicators. next step is giving todays prediction and writing control function for optimizing and predicting multiple stocks

--- MATLAB/main.m
+++ MATLAB/main.m
@@ -1,4 +1,4 @@
-function [perc] = main(symbol, varargin)
+function main(symbol, varargin)
 p = inputParser;
 addRequired(p, 'symbol');
 addOptional(p, 'RSI_LENGTH', 14);
@@ -11,11 +11,14 @@ AROON_LENGTH = p.Results.AROON_LENGTH;
 MACD_LONG = p.Results.MACD_LONG;
 MACD_SHORT = floor(MACD_LONG / 1.8);
 MACD_SIG = floor(MACD_SHORT / 1.5);
-% symbol
 
-SIMULATION_LENGTH = 365;
 
 data = webread(strcat('http://real-chart.finance.yahoo.com/table.csv?s=',symbol));
+if height(data) >= 365
+  SIMULATION_LENGTH = 365;
+else
+  SIMULATION_LENGTH = height(data);
+end
 data = data(1:SIMULATION_LENGTH,:);
 dates = fliplr(datenum(table2array(data(:,1)))');
 open = fliplr(table2array(data(:,2))');
@@ -25,73 +28,59 @@ close = fliplr(table2array(data(:,5))');
 volume = fliplr(table2array(data(:,6))');
 adjClose = fliplr(table2array(data(:,7))');
 
-% CONSTANTS
-BUY_THRESHOLD = 0.4;
-SELL_THRESHOLD = -0.4;
-
-prediction = zeros(1, SIMULATION_LENGTH);
-[null, RSI_predictions] = RSI(close, RSI_LENGTH);
-[null, null, arron, aroon_predictions] = aroon(close, AROON_LENGTH);
-[null, null, macd_predictions] = MACD(close, MACD_LONG, MACD_SHORT, MACD_SIG);
-
-predictors(1, :) = RSI_predictions;
-predictors(end, :) = aroon_predictions;
-predictors(end, :) = macd_predictions;
-
-MACD_WEIGHT_X = 0;
-AROON_WEIGHT_X = 0;
-RSI_WEIGHT_X = 0;
-
-fundsCurrent = 100000;
-sharesCurrent = 0;
-fundsHeld = 0;
-
-for i  = [1:SIMULATION_LENGTH]
-  % -- CALCULATE WEIGHTS
-  % MACD_WEIGHT = (1 / (1 + (exp(-MACD_WEIGHT_X))));
-  % AROON_WEIGHT = (1 / (1 + (exp(-AROON_WEIGHT_X))));
-  % RSI_WEIGHT = (1 / (1 + (exp(-RSI_WEIGHT_X))));
-
-  % -- DETERMINE PREDICTION
-  prediction(i) = mean(predictors(:,i));
-
-  % -- BUY
-  if prediction(i) >= BUY_THRESHOLD
-    if sharesCurrent >= 0
-      sharesCurrent = sharesCurrent + floor(fundsCurrent / close(i));
-      fundsCurrent = fundsCurrent -  floor(fundsCurrent / close(i)) * close(i);
-    end
-  % -- SELL
-  elseif prediction (i) <= SELL_THRESHOLD
-    if sharesCurrent > 0
-      fundsCurrent = fundsCurrent + sharesCurrent * close(i);
-      sharesCurrent = 0;
-    end
+actuals = zeros(1,SIMULATION_LENGTH);
+avgLength = floor(SIMULATION_LENGTH/20);
+for i = [avgLength+1:SIMULATION_LENGTH]
+  if mean(close(i-avgLength:i)) > close(i)
+    actuals(i) = 1;
+  elseif mean(close(i-avgLength:i)) < close(i)
+    actuals(i) = -1;
+  else
+    actuals(i) = 0;
   end
-
-  % -- UPDATE PORTFOLIO
-  funds(i) = fundsCurrent - fundsHeld;
-  shares(i) = sharesCurrent;
-  value(i) = funds(i) + shares(i) * close(i);
-  perc(i) = (value(i) - 100000) / 100000 * 100;
-
-  % -- REWEIGHT PREDICTORS
-  % if i > floor(SIMULATION_LENGTH / 20)
-  %   if (macd_predictions(i) > 0 & mean(close(i - floor(SIMULATION_LENGTH / 20): i)) > close(i)) || macd_predictions(i) < 0 & mean(close(i - floor(SIMULATION_LENGTH / 20): i)) < close(i)
-  %     MACD_WEIGHT_X = MACD_WEIGHT_X + 0.1;
-  %   elseif (macd_predictions(i) > 0 & mean(close(i - floor(SIMULATION_LENGTH / 20): i)) < close(i)) || macd_predictions(i) < 0 & mean(close(i - floor(SIMULATION_LENGTH / 20): i)) > close(i)
-  %     MACD_WEIGHT_X = MACD_WEIGHT_X - 0.1;
-  %   end
-  %   if (aroon_predictions(i) > 0 & mean(close(i - floor(SIMULATION_LENGTH / 20): i)) > close(i)) || aroon_predictions(i) < 0 & mean(close(i - floor(SIMULATION_LENGTH / 20): i)) < close(i)
-  %     AROON_WEIGHT_X = AROON_WEIGHT_X + 0.1;
-  %   elseif (aroon_predictions(i) > 0 & mean(close(i - floor(SIMULATION_LENGTH / 20): i)) < close(i)) || aroon_predictions(i) < 0 & mean(close(i - floor(SIMULATION_LENGTH / 20): i)) > close(i)
-  %     AROON_WEIGHT_X = AROON_WEIGHT_X - 0.1;
-  %   end
-  %   if (RSI_predictions(i) > 0 & mean(close(i - floor(SIMULATION_LENGTH / 20): i)) > close(i)) || RSI_predictions(i) < 0 & mean(close(i - floor(SIMULATION_LENGTH / 20): i)) < close(i)
-  %     RSI_WEIGHT_X = RSI_WEIGHT_X + 0.1;
-  %   elseif (RSI_predictions(i) > 0 & mean(close(i - floor(SIMULATION_LENGTH / 20): i)) < close(i)) || RSI_predictions(i) < 0 & mean(close(i - floor(SIMULATION_LENGTH / 20): i)) > close(i)
-  %     RSI_WEIGHT_X = RSI_WEIGHT_X - 0.1;
-  %   end
-  % end
 end
-perc = perc(SIMULATION_LENGTH);
+
+r = zeros(1,21);
+parfor RSI_LENGTH = [7:21]
+  [null, RSI_predictions] = RSI(close, RSI_LENGTH);
+  r(RSI_LENGTH) = length(find(RSI_predictions == actuals));
+end
+[M,I] = max(r);
+rOpt = I;
+
+r = zeros(1,30);
+parfor AROON_LENGTH = [10:30]
+  [null, null, arron, aroon_predictions] = aroon(close, AROON_LENGTH);
+  r(AROON_LENGTH) = length(find(aroon_predictions == actuals));
+end
+[M,I] = max(r);
+aOpt = I;
+
+r = zeros(1,39);
+parfor MACD_LONG = [13:39]
+  MACD_SHORT = floor(MACD_LONG / 1.8);
+  MACD_SIG = floor(MACD_SHORT / 1.5);
+  [null, null, macd_predictions] = MACD(close, MACD_LONG, MACD_SHORT, MACD_SIG);
+  r(MACD_LONG) = length(find(macd_predictions == actuals));
+end
+[M,I] = max(r);
+mOpt = I;
+
+
+
+
+
+
+
+
+
+
+
+% prediction = zeros(1, SIMULATION_LENGTH);
+% [null, RSI_predictions] = RSI(close, RSI_LENGTH);
+% [null, null, arron, aroon_predictions] = aroon(close, AROON_LENGTH);
+% [null, null, macd_predictions] = MACD(close, MACD_LONG, MACD_SHORT, MACD_SIG);
+%
+% predictors(1, :) = RSI_predictions;
+% predictors(end, :) = aroon_predictions;
+% predictors(end, :) = macd_predictions;
