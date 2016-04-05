03f7ebab26f15335f97b29bd8c260096d1030b1c
hoover456 <hoover852@yahoo.com>
2016-04-04 21:43:00 -0400

    Moving to MATLAB part 2

--- main.m
+++
@@ -1,8 +0,0 @@
-
-%MAIN Summary of this function goes here
-%   Detailed explanation goes here
-%symbol = input('test: ', 's');
-symbol = 'NUGT';
-data = webread(strcat('http://real-chart.finance.yahoo.com/table.csv?s=',symbol));
-test = fints(table2array(data(2:end,1)), table2array(data(2:end,2:end),'VariableNames',{'Date' 'Open' 'High' 'Low' 'Close' 'Volume' 'AdjClose'}));
-test = chfield(test, {'series1' 'series2' 'series3' 'series4' 'series5' 'series6'}, {'Open' 'High' 'Low' 'Close' 'Volume' 'AdjClose'});
--- test.py
+++
@@ -1,8 +0,0 @@
-class test:
-    def notMain(self):
-        print('BLAH')
-
-    def main():
-        notMain()
-    if __name__ == '__main__':
-        main()
