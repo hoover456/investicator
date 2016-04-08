a7c87358317f2b46629bd19e988d38ee1d53913b
Andrew <hoover852@gmail.com>
2016-04-07 22:14:26 -0400

    cleaned list of tradable businesses

--- MATLAB/main.m
+++ MATLAB/main.m
@@ -27,52 +27,55 @@ try
   close = fliplr(table2array(data(:,5))');
   volume = fliplr(table2array(data(:,6))');
   adjClose = fliplr(table2array(data(:,7))');
-
-  actuals = zeros(1,SIMULATION_LENGTH);
-  avgLength = floor(SIMULATION_LENGTH/20);
-  for i = [avgLength+1:SIMULATION_LENGTH]
-    if mean(close(i-avgLength:i)) > close(i)
-      actuals(i) = 1;
-    elseif mean(close(i-avgLength:i)) < close(i)
-      actuals(i) = -1;
-    else
-      actuals(i) = 0;
+  if close(end) == 0
+    suggestion = -2;
+  else
+    actuals = zeros(1,SIMULATION_LENGTH);
+    avgLength = floor(SIMULATION_LENGTH/20);
+    for i = [avgLength+1:SIMULATION_LENGTH]
+      if mean(close(i-avgLength:i)) > close(i)
+        actuals(i) = 1;
+      elseif mean(close(i-avgLength:i)) < close(i)
+        actuals(i) = -1;
+      else
+        actuals(i) = 0;
+      end
     end
+
+    % steps =
+
+
+    r = zeros(1,21);
+    parfor RSI_LENGTH = [7:21]
+      [null, RSI_predictions] = RSI(close, RSI_LENGTH);
+      r(RSI_LENGTH) = length(find(RSI_predictions == actuals));
+    end
+    [M,I] = max(r);
+    rOpt = I;
+
+    r = zeros(1,30);
+    parfor AROON_LENGTH = [10:30]
+      [null, null, arron, aroon_predictions] = aroon(close, AROON_LENGTH);
+      r(AROON_LENGTH) = length(find(aroon_predictions == actuals));
+    end
+    [M,I] = max(r);
+    aOpt = I;
+
+    r = zeros(1,39);
+    parfor MACD_LONG = [13:39]
+      MACD_SHORT = floor(MACD_LONG / 1.8);
+      MACD_SIG = floor(MACD_SHORT / 1.5);
+      [null, null, macd_predictions] = MACD(close, MACD_LONG, MACD_SHORT, MACD_SIG);
+      r(MACD_LONG) = length(find(macd_predictions == actuals));
+    end
+    [M,I] = max(r);
+    mOpt = I;
+
+    [null, RSI_predictions] = RSI(close, rOpt);
+    [null, null, null, aroon_predictions] = aroon(close, aOpt);
+    [null, null, macd_predictions] = MACD(close, mOpt, floor(mOpt / 1.8), floor(floor(mOpt / 1.8) / 1.5));
+    suggestion = (RSI_predictions(end) + aroon_predictions(end) + macd_predictions(end)) / 3;
   end
-
-  % steps =
-
-
-  r = zeros(1,21);
-  parfor RSI_LENGTH = [7:21]
-    [null, RSI_predictions] = RSI(close, RSI_LENGTH);
-    r(RSI_LENGTH) = length(find(RSI_predictions == actuals));
-  end
-  [M,I] = max(r);
-  rOpt = I;
-
-  r = zeros(1,30);
-  parfor AROON_LENGTH = [10:30]
-    [null, null, arron, aroon_predictions] = aroon(close, AROON_LENGTH);
-    r(AROON_LENGTH) = length(find(aroon_predictions == actuals));
-  end
-  [M,I] = max(r);
-  aOpt = I;
-
-  r = zeros(1,39);
-  parfor MACD_LONG = [13:39]
-    MACD_SHORT = floor(MACD_LONG / 1.8);
-    MACD_SIG = floor(MACD_SHORT / 1.5);
-    [null, null, macd_predictions] = MACD(close, MACD_LONG, MACD_SHORT, MACD_SIG);
-    r(MACD_LONG) = length(find(macd_predictions == actuals));
-  end
-  [M,I] = max(r);
-  mOpt = I;
-
-  [null, RSI_predictions] = RSI(close, rOpt);
-  [null, null, null, aroon_predictions] = aroon(close, aOpt);
-  [null, null, macd_predictions] = MACD(close, mOpt, floor(mOpt / 1.8), floor(floor(mOpt / 1.8) / 1.5));
-  suggestion = (RSI_predictions(end) + aroon_predictions(end) + macd_predictions(end)) / 3;
 catch exception
   fprintf('Could Not Retrieve Data for %s, please remove from list\n', symbol)
   suggestion = -2;
